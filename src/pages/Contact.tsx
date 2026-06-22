6e1bff2b854e456706264ba42215269db5092715
Salim <salimbd.ga@gmail.com>
2026-06-22 11:12:56 +0600

    Create Contact.tsx

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Contact Us</h1>
+      <p>Email: salimbd.ga@gmail.com</p>
+    
+    </div>
+  );
+};
+
+export default Contact;
