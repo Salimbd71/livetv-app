ce067cea879f620afc6829399e21546d157aa301
Salim <salimbd.ga@gmail.com>
2026-06-22 11:21:21 +0600

    Add About page component

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">About Us</h1>
+      <p>LiveTV is a streaming platform for live channels.</p>
+    </div>
+  );
+};
+
+export default About;
