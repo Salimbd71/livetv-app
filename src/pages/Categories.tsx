0d2d5dcb64d22534d1f45479edfc004f8729d9b0
Salim <salimbd.ga@gmail.com>
2026-06-22 11:23:01 +0600

    Add Categories component to display channel categories

---
+++ src/pages/Categories.tsx
@@ -0,0 +1,10 @@
+const Categories = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Categories</h1>
+      <p>All channel categories will show here</p>
+    </div>
+  );
+};
+
+export default Categories;
