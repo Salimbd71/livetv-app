be4c6ba1f3b6854b90e3eaa7d0151ae1253b3067
Salim <salimbd.ga@gmail.com>
2026-06-22 12:11:59 +0600

    Update categories description to include 'Click to Play'

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -2,7 +2,7 @@ const Categories = () => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Categories</h1>
-      <p>All channel categories will show here</p>
+      <p>All channel categories will show here. Click to Play</p>
     </div>
   );
 };
