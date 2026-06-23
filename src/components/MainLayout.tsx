c8f83f38d7af77ac604d697e5f0bef4dbf42c55a
Salim <salimbd.ga@gmail.com>
2026-06-23 19:11:38 +0600

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar"; // আপনার Navbar ফাইলের সঠিক পাথ দিন
+import Navbar from "./Navbar"; // ⚠️ পরিবর্তন: একই ফোল্ডারে থাকায় শুধু "./Navbar" হবে
 
 const MainLayout = () => {
   return (
@@ -7,7 +7,7 @@ const MainLayout = () => {
       {/* Navbar সব পেজের ওপরে থাকবে */}
       <Navbar /> 
       
-      {/* Outlet এর জায়গায় একেক পেজে একেক কম্পোনেন্ট (যেমন: Index, About) লোড হবে */}
+      {/* Outlet এর জায়গায় একেক পেজে একেক কম্পোনেন্ট লোড হবে */}
       <main>
         <Outlet />
       </main>
