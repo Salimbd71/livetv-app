9ee96f8a6e09ae2f02b80816b8da5b3d515b1041
Salim <salimbd.ga@gmail.com>
2026-06-24 19:34:11 +0600

    Set video volume to specified level

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -99,7 +99,7 @@ const VideoPlayer = ({
     destroyPlayer();
     
     
-    video.volume = 0; // new add
+    video.volume = volume; // new add
     setError(null);
     setLoading(true);
     setIsPlaying(true);
@@ -119,7 +119,7 @@ const VideoPlayer = ({
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
   setLoading(false);
   video.muted = false; // 👈 প্রথমবার ট্রু করে দিন, অথবা ইউজারকে 'Click to Play' বাটন দেখান
-
+  video.volume = volume; // new add
   video.play().catch(() => {});
 });
 
