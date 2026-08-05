2d5e89fdc883b380fe324ef66252c3cea9f74cbf
Salim <salimbd.ga@gmail.com>
2026-08-05 21:19:46 +0600

    Add files via upload

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -113,7 +113,7 @@ export function Navbar() {
                 value={searchQuery}
                 onChange={setSearchQuery}
                 onClear={() => setSearchQuery("")}
-                placeholder={location === "/movies" ? t("Search movies...") : t("Search channels...")}
+                placeholder={t("Search channels...")}
                 testId="input-search"
               />
             </div>
@@ -147,7 +147,7 @@ export function Navbar() {
               value={searchQuery}
               onChange={setSearchQuery}
               onClear={() => setSearchQuery("")}
-              placeholder={location === "/movies" ? t("Search movies...") : t("Search channels...")}
+              placeholder={t("Search channels...")}
               testId="input-search-mobile"
             />
           </div>
